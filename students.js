29947ac4fd3fd74bd40556993095e31d8ef7c599
Karina Tocknell <karina.cove@education.vic.gov.au>
2026-02-19 22:13:34 +0800

    Update students.js

--- students.js
+++ students.js
@@ -1,9 +1,9 @@
 // students.js
 const classData = {
-  "Year 3": ["Jack DV","Adele F","Molly G","Lachlan O","Levi S","Roman S","Alexander W","Gabby W", "Blair Y"],
+  "Year 3": ["Jack DV","Adele F","Molly G","Lachlan O","Levi S","Roman S","Alexander W","Gabby W","Blair Y"],
   "Year 4:" ["Hunter B","Legion B","Zeilyn B","Raine C","Neve C","Dejane' dG","Hailey DV","Seth F","James H","Riley J","Elliot H","Penelope K","Wairere K-W","Ollie O","Erin R","Jack S","Peyton T","Aaron T","Olivia W","Sienna W"],
   "Year 5": ["Elkie B","Pippa C","Rylan C","Rocco F","Oliver L","Grace L","Oscar M","Angela P","Jye P","Maya P","Annie R","James S","Mia S","Sophia T"],
-  "Year 6": ["Theo C","Jayden C","Alfie C","Avayah C","Maya DV", "Sofie D","Olivia F","Max G","Eimear H","Zach M","Lucas O","Gavin R","Chase S","Nicole S","Harrison T","Valentina W","Ella W"],
+  "Year 6": ["Theo C","Jayden C","Alfie C","Avayah C","Maya DV","Sofie D","Olivia F","Max G","Eimear H","Zach M","Lucas O","Gavin R","Chase S","Nicole S","Harrison T","Valentina W","Ella W"],
   "Year 7": ["Spencer B","Torana B","Ricardo B","Cohen C","Jennifer J","Indianna M","Benji M","Paige P","Macey P","Toby S","Charis T","Ava W","Miekenzie W"],
   "Year 8": ["Piper C","Gemma F","Lyla H","Imogen H","Kobi L","Eira P","Aden S","Kate S","Nico W"],
   "Year 9": ["Toby B","Jeremy B","Macy C","Marley J","Dakoda M","Rhave P","Rhienna P","Kira S","Noah S","Phoebe T","David V","Abbygale VR","Seth W"],
